c6f441671f71ff95ae0e2a38187592e73f8a7547
AsherS61 <ashiraboonfongmanee@gmail.com>
2025-11-25 12:12:05 +0700

    fix: login

--- frontend/app/auth/signin/page.tsx
+++ frontend/app/auth/signin/page.tsx
@@ -1,10 +1,18 @@
 'use client'
 
-import { signIn } from 'next-auth/react';
+import { signIn, useSession } from 'next-auth/react';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 export default function SignInPage() {
-    return (
+    const router = useRouter();
+    const { data: session } = useSession();
+
+    if (session) {
+        router.push('/');
+    }
+
+    return(
         <div className="relative flex flex-col items-center justify-center min-h-screen w-full bg-gradient-to-br from-blue-50 via-white to-indigo-50">
             {/* Decorative background elements */}
             <div className="absolute inset-0 overflow-hidden pointer-events-none">
@@ -50,12 +58,13 @@ export default function SignInPage() {
                             fill="currentColor"
                             xmlns="http://www.w3.org/2000/svg"
                         >
+                            {/*Line Icon PATH MANN SOO LONGGG*/}
                             <path d="M19.365 9.863c.349 0 .63.285.63.631 0 .345-.281.63-.63.63H17.61v1.125h1.755c.349 0 .63.283.63.63 0 .344-.281.629-.63.629h-2.386c-.345 0-.627-.285-.627-.629V8.108c0-.345.282-.63.63-.63h2.386c.346 0 .627.285.627.63 0 .349-.281.63-.63.63H17.61v1.125h1.755zm-3.855 3.016c0 .27-.174.51-.432.596-.064.021-.133.031-.199.031-.211 0-.391-.09-.51-.25l-2.443-3.317v2.94c0 .344-.279.629-.631.629-.346 0-.626-.285-.626-.629V8.108c0-.27.173-.51.43-.595.06-.023.136-.033.194-.033.195 0 .375.104.495.254l2.462 3.33V8.108c0-.345.282-.63.63-.63.345 0 .63.285.63.63v4.771zm-5.741 0c0 .344-.282.629-.631.629-.345 0-.627-.285-.627-.629V8.108c0-.345.282-.63.63-.63.346 0 .628.285.628.63v4.771zm-2.466.629H4.917c-.345 0-.63-.285-.63-.629V8.108c0-.345.285-.63.63-.63.348 0 .63.285.63.63v4.141h1.756c.348 0 .629.283.629.63 0 .344-.282.629-.629.629M24 10.314C24 4.943 18.615.572 12 .572S0 4.943 0 10.314c0 4.811 4.27 8.842 10.035 9.608.391.082.923.258 1.058.59.12.301.079.766.038 1.08l-.164 1.02c-.045.301-.24 1.186 1.049.645 1.291-.539 6.916-4.078 9.436-6.975C23.176 14.393 24 12.458 24 10.314"/>
                         </svg>
                         <span>Sign in with LINE</span>
                     </button>
 
-                    {/* Additional info */}
+                    {/* Additional info WHERE IS MY ENDFIELD */}
                     <div className="mt-8 text-center">
                         <p className="text-sm text-gray-500">
                             By continuing, you agree to our{' '}
@@ -66,10 +75,10 @@ export default function SignInPage() {
                     </div>
                 </div>
 
-                {/* Footer text */}
+                {/* Footer text DAMN I NEED THAT BETA*/}
                 <p className="text-center text-gray-600 text-sm mt-6">
                     Need help?{' '}
-                    <a href="#" className="text-blue-600 hover:text-blue-700 font-medium">
+                    <a href="https://www.instagram.com/daykungza" className="text-blue-600 hover:text-blue-700 font-medium">
                         Contact Support
                     </a>
                 </p>
