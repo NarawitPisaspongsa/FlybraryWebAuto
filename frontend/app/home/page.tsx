80bdffc3eeb64bf94179acccc89ec9dfa77215f9
AsherS61 <ashiraboonfongmanee@gmail.com>
2025-11-28 20:33:19 +0700

    help13

--- frontend/app/home/page.tsx
+++ frontend/app/home/page.tsx
@@ -2,14 +2,15 @@
 
 // import next
 import { useRouter } from 'next/navigation'
-import BannerSection from './components/BannerSection'
 import { useSession } from 'next-auth/react'
+import { getServerSession } from 'next-auth'
+import { handler } from '../api/auth/[...nextauth]/route';
 
 export default function Home() {
   // Primary variables
   const router = useRouter()
 
-  const { data: session } = useSession();
+  const session = getServerSession(handler);
   console.log("USER PROFILE: ", session)
 
   return (
