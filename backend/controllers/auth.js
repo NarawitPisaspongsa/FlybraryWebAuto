781732a66cfd116637d80a09bb29a95ccc19ff21
NarawitPisaspongsa <6733123921@student.chula.ac.th>
2025-12-05 13:54:03 +0700

    hope

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -8,15 +8,26 @@ exports.login = async (req, res, next) => {
     const user = await User.findOne({
       lineId: lineId
     });
-    
-    if(!user) {
-      const created_user = await User.create({
-        name:displayName,
-        lineId:lineId,
-        profile:picture,
-        role:'user'
+    if (user) {
+      return res.status(200).json({
+        success: true,
+        data: user
       });
     }
+    const created_user = await User.create({
+      name:displayName,
+      lineId:lineId,
+      profile:picture,
+      role:'user'
+    });
+
+    if (created_user) {
+      return res.status(200).json({
+      success: true,
+      data: created_user
+      })
+    }
+
   } catch (err) {
     res.status(400).json({success: false, message: "bad req"});
     console.log(err);
