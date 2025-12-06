0e3d881116804ed99dc2d8f8f0e3f58ed75d4098
AsherS61 <ashiraboonfongmanee@gmail.com>
2025-12-06 14:50:37 +0700

    rollback:

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const connectDB = require("./config/db");
+const cors = require('cors')
+
+dotenv.config({ path: "./.env" });
+
+connectDB();
+
+const auth = require('./routes/auth.js'); 
+const books = require('./routes/books.js');
+const transactions = require('./routes/transactions.js');
+
+const app = express();
+
+//body parser
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+app.use(cors())
+
+//Cookie parser
+app.use(cookieParser());
+
+//Mount routers
+app.use("/api/v1/books", books);
+app.use("/api/v1/auth", auth); 
+app.use("/api/v1/transactions", transactions);
+
+module.exports = app;
