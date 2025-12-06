805d6ae6ee494f004bb429d3e4d1361fb19936e0
AsherS61 <ashiraboonfongmanee@gmail.com>
2025-12-06 15:16:57 +0700

    fix: cors

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,13 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-app.use(cors())
+app.use(cors({
+  origin: [
+    'https://flybrary-web-auto-frontend.vercel.app',
+    'http://localhost:3000'
+  ],
+  credentials: true
+}));
 
 //Cookie parser
 app.use(cookieParser());
