1b3b2b780b41639e97e656e7902c7f8bddc62f69
AsherS61 <ashiraboonfongmanee@gmail.com>
2025-12-07 09:44:37 +0700

    add: limit to all get transactions

--- backend/controllers/transaction.js
+++ backend/controllers/transaction.js
@@ -71,7 +71,7 @@ exports.getTransaction = async (req, res, next) => {
 exports.getTransactionsByUser = async (req, res, next) => {
     try {
         const userId = req.params.id;
-        const transactions = await Transaction.find({ user: userId }).populate('book');
+        const transactions = await Transaction.find({ user: userId }).sort({ createdAt: -1 }).limit(10).populate('book');
 
         if (!transactions) {
             return res.status(404).json({
@@ -108,7 +108,7 @@ exports.getTransactionsByUser = async (req, res, next) => {
 exports.getTransactionsByBook = async (req, res, next) => {
     try {
         const bookId = req.params.id;
-        const transactions = await Transaction.find({ book: bookId });
+        const transactions = await Transaction.find({ book: bookId }).sort({ createdAt: -1 }).limit(10);
 
         if (!transactions) {
             return res.status(404).json({
